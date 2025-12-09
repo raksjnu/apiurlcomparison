3916195ed4b737dce1d841119ded7860721dd38f
raksjnu <72746690+raksjnu@users.noreply.github.com>
2025-12-09 10:42:52 -0600

    sh file

--- start-gui.sh
+++ start-gui.sh
@@ -5,8 +5,8 @@ echo "Starting API Response Comparison Tool - GUI..."
 echo "The GUI will open automatically in your default browser."
 echo ""
 
-# Run the JAR file without config to start GUI mode
-java -jar target/apiurlcomparison-1.0.0.jar
+# Run the GUI web server by explicitly specifying the ApiUrlComparisonWeb class
+java -cp target/apiurlcomparison-1.0.0.jar com.myorg.apiurlcomparison.ApiUrlComparisonWeb
 
 # Keep the terminal open if there's an error
 if [ $? -ne 0 ]; then
